8bfad4fc72a38021a3f97f5eb3dde7cde0f7439e
trabyroblox-wq <trabyroblox@gmail.com>
2026-02-02 17:59:52 -0500

    Update security.js

--- backend/src/middleware/security.js
+++ backend/src/middleware/security.js
@@ -1,1 +1,21 @@
+const helmet = require('helmet');
+const cors = require('cors');
+const config = require('../../config/default');
 
+module.exports = (app) => {
+  app.use(helmet({
+    contentSecurityPolicy: false, // CSP often breaks proxied content
+    referrerPolicy: { policy: 'no-referrer-when-downgrade' }
+  }));
+
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || config.allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
+  }));
+};
