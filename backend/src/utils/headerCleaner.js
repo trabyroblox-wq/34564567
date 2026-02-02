899aa97c1ef1379928ed6aea8e47414b770b69de
trabyroblox-wq <trabyroblox@gmail.com>
2026-02-02 18:00:50 -0500

    Update headerCleaner.js

--- backend/src/utils/headerCleaner.js
+++ backend/src/utils/headerCleaner.js
@@ -1,1 +1,11 @@
-
+module.exports = (headers) => {
+  const clean = { ...headers };
+  const remove = [
+    'host', 'connection', 'proxy-connection', 'proxy-authorization',
+    'x-forwarded-for', 'x-forwarded-host', 'x-forwarded-proto',
+    'cf-connecting-ip', 'true-client-ip', 'x-real-ip'
+  ];
+  
+  remove.forEach(key => delete clean[key.toLowerCase()]);
+  return clean;
+};
