fc4e884abfbe9399b46b590c19f6404147f4ac7f
trabyroblox-wq <trabyroblox@gmail.com>
2026-02-02 18:00:05 -0500

    Update rateLimit.js

--- backend/src/middleware/rateLimit.js
+++ backend/src/middleware/rateLimit.js
@@ -1,1 +1,18 @@
+const { RateLimiterMemory } = require('rate-limiter-flexible');
+const config = require('../../config/default');
 
+const limiter = new RateLimiterMemory({
+  points: config.rateLimit.points,
+  duration: config.rateLimit.duration,
+});
+
+module.exports = (req, res, next) => {
+  limiter.consume(req.ip)
+    .then(() => next())
+    .catch(() => {
+      res.status(429).json({
+        error: 'Too Many Requests',
+        retryAfter: '60 seconds'
+      });
+    });
+};
